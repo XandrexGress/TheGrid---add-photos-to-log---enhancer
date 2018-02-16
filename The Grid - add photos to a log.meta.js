e4106619927e60ac81d81e2fe76f2b642733e51c
XandrexGress <36522702+XandrexGress@users.noreply.github.com>
2018-02-16 01:39:34 +0100

    Properly manage 'user scripts' headers (#1)
    * change headers to comply with UserScripts update mechanism
    
    add the updateURL and downloadURL using RAWGIT.

--- The Grid - add photos to a log.meta.js
+++ The Grid - add photos to a log.meta.js
@@ -1,9 +1,13 @@
 // ==UserScript==
-// @name         The Grid : add photos to a log
-// @namespace    https://the-grid.org/
-// @version      2
-// @description  change UI for this page
-// @author       Telegram: @Xandrex / IGN: XandrexGress
-// @include		https://the-grid.org/r/?page=uploadphoto&*
-// @include		https://the-grid.org/r/?page=uploadphoto&id=2109896&feature=53722
+// @id             TheGrid-add-photos-to-a-log-enhancer@XandrexGress
+// @name           TheGrid user script : "add photos to a log" page enhancer
+// @category       User Script
+// @version        03.2018-02-16
+// @namespace      https://the-grid.org/
+// @updateURL      https://cdn.rawgit.com/XandrexGress/TheGrid---add-photos-to-log---enhancer/30e39724/The%20Grid%20-%20add%20photos%20to%20a%20log.meta.js
+// @downloadURL    https://cdn.rawgit.com/XandrexGress/TheGrid---add-photos-to-log---enhancer/30e39724/The%20Grid%20-%20add%20photos%20to%20a%20log.user.js
+// @description    [2018-02-16-03] changes layout of the pictures grid, thumbnails are not cropped anymore
+// @author         Telegram: @Xandrex / IGN: XandrexGress
+// @include        https://the-grid.org/r/?page=uploadphoto&*
+// @grant          none 
 // ==/UserScript==
