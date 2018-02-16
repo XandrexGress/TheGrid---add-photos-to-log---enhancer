30e397240a26220b4ca577db4284fdae646d11d2
XandrexGress <36522702+XandrexGress@users.noreply.github.com>
2018-02-16 01:14:59 +0100

    Create The Grid - add photos to a log.meta.js

---
+++ The Grid - add photos to a log.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         The Grid : add photos to a log
+// @namespace    https://the-grid.org/
+// @version      2
+// @description  change UI for this page
+// @author       Telegram: @Xandrex / IGN: XandrexGress
+// @include		https://the-grid.org/r/?page=uploadphoto&*
+// @include		https://the-grid.org/r/?page=uploadphoto&id=2109896&feature=53722
+// ==/UserScript==
